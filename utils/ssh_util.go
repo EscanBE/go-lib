0e1ccc4d9927e5275b58255a7d986cbd3dda72e2
VictorTrustyDev <me@victortrusty.dev>
2022-11-20 13:37:22 +0700

    add SSH utils

--- utils/ssh_util.go
+++ utils/ssh_util.go
@@ -6,40 +6,36 @@ import (
 	"github.com/EscanBE/go-lib/types"
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/ssh"
-	"net"
 	"strconv"
 )
 
-func ExecuteRemoteCommandViaSSH(remoteCommand, remoteHost, sshPort string, sshAuth types.SshAuthPrivateKey) (string, error) {
-	if len(remoteCommand) < 1 || IsBlank(remoteCommand) {
-		return "", fmt.Errorf("no command was provided")
+// ExecuteRemoteCommandViaSSH performs execution of remote command on target server,
+// it returns the output string of the execution and an error if any
+func ExecuteRemoteCommandViaSSH(remoteCommand string, remoteServer *types.SshRemote) (string, error) {
+	if remoteServer.IsAuthByPrivateKey() {
+		return executeRemoteCommandViaSSHUsingPrivateKey(remoteCommand, remoteServer)
 	}
-	if len(remoteHost) < 1 || IsBlank(remoteHost) {
-		return "", fmt.Errorf("no remote host was provided")
+
+	return "", fmt.Errorf("not supported authentication type or none provided")
+}
+
+// executeRemoteCommandViaSSHUsingPrivateKey performs execution of remote command on target server,
+// using private key authentication method,
+// it returns the output string of the execution and an error if any
+func executeRemoteCommandViaSSHUsingPrivateKey(remoteCommand string, remoteServer *types.SshRemote) (string, error) {
+	if remoteServer.AuthByPrivateKey == nil {
+		return "", fmt.Errorf("not a SSH auth by private key (not supplied)")
 	}
-	if len(sshPort) < 1 || IsBlank(sshPort) {
-		return "", fmt.Errorf("no SSH port was provided")
-	}
-	portNo, errPort := strconv.ParseInt(sshPort, 10, 32)
-	if errPort != nil {
-		return "", errors.Wrap(errPort, "supplied SSH port is invalid format")
-	}
-	if portNo < 1 || portNo > 65535 {
-		return "", fmt.Errorf("bad SSH port no.%d", portNo)
-	}
-	if len(sshAuth.UserName) < 1 || IsBlank(sshAuth.UserName) {
-		return "", fmt.Errorf("no username was provided")
-	}
-	if len(sshAuth.PrivateKey) < 24 {
+	if len(remoteServer.AuthByPrivateKey.PrivateKey) < 24 {
 		return "", fmt.Errorf("bad private key")
 	}
 
 	var key ssh.Signer
 	var err error
-	if len(sshAuth.EncryptionPassword) > 0 {
-		key, err = ssh.ParsePrivateKeyWithPassphrase(sshAuth.PrivateKey, sshAuth.EncryptionPassword)
+	if len(remoteServer.AuthByPrivateKey.Passphrase) > 0 {
+		key, err = ssh.ParsePrivateKeyWithPassphrase(remoteServer.AuthByPrivateKey.PrivateKey, remoteServer.AuthByPrivateKey.Passphrase)
 	} else {
-		key, err = ssh.ParsePrivateKey(sshAuth.PrivateKey)
+		key, err = ssh.ParsePrivateKey(remoteServer.AuthByPrivateKey.PrivateKey)
 	}
 	if err != nil {
 		return "", errors.Wrap(err, "failed to decrypt SSH private key")
@@ -48,17 +44,45 @@ func ExecuteRemoteCommandViaSSH(remoteCommand, remoteHost, sshPort string, sshAu
 		ssh.PublicKeys(key),
 	}
 
+	return executeRemoteCommandViaSSH(remoteCommand, remoteServer, auth)
+}
+
+// executeRemoteCommandViaSSHUsingPrivateKey performs execution of remote command on target server,
+// using private key authentication method,
+// it returns the output string of the execution and an error if any
+func executeRemoteCommandViaSSH(remoteCommand string, remoteServer *types.SshRemote, auth []ssh.AuthMethod) (string, error) {
+	if IsBlank(remoteCommand) {
+		return "", fmt.Errorf("no command was provided")
+	}
+	if IsBlank(remoteServer.Host) {
+		return "", fmt.Errorf("no remote host was provided")
+	}
+	if IsBlank(remoteServer.Port) {
+		return "", fmt.Errorf("no SSH port was provided")
+	}
+	portNo, errPort := strconv.ParseInt(remoteServer.Port, 10, 32)
+	if errPort != nil {
+		return "", errors.Wrap(errPort, "supplied SSH port is invalid format")
+	}
+	if portNo < 1 || portNo > 65535 {
+		return "", fmt.Errorf("bad SSH port no.%d", portNo)
+	}
+	if IsBlank(remoteServer.Username) {
+		return "", fmt.Errorf("no username was provided")
+	}
+
 	// Authentication
 	config := &ssh.ClientConfig{
-		User: sshAuth.UserName,
+		User: remoteServer.Username,
 		// https://github.com/golang/go/issues/19767
 		// as clientConfig is non-permissive by default
 		// you can set ssh.InsercureIgnoreHostKey to allow any host
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 		Auth:            auth,
 	}
+
 	// Connect
-	client, err := ssh.Dial("tcp", net.JoinHostPort(remoteHost, sshPort), config)
+	client, err := ssh.Dial("tcp", remoteServer.GetEndpoint(), config)
 	if err != nil {
 		return "", errors.Wrap(err, "failed to connect to remote server")
 	}
@@ -79,4 +103,13 @@ func ExecuteRemoteCommandViaSSH(remoteCommand, remoteHost, sshPort string, sshAu
 	// Finally, run the command
 	err = session.Run(remoteCommand)
 	return b.String(), err
+}
+
+func IsPassphraseCanDecryptPrivateKey(privateKey, passphrase []byte) (bool, error) {
+	_, err := ssh.ParsePrivateKeyWithPassphrase(privateKey, passphrase)
+	if err == nil {
+		return true, nil
+	} else {
+		return false, err
+	}
 }
