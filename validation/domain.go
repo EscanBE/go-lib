5f835debad190f4ebdaaac940be8a0746c4fa157
VictorTrustyDev <me@victortrusty.dev>
2022-11-21 23:56:37 +0700

    on dev v0.0.5 with test

--- validation/domain.go
+++ validation/domain.go
@@ -2,14 +2,14 @@ package validation
 
 import (
 	"golang.org/x/net/idna"
-	"strings"
 )
 
-// IsValidHostname returns true if input hostname is localhost or valid domain name
+// IsValidHostname returns true if input hostname is a valid domain name.
+// with google.com => true,
+// with localhost => true,
+// with https://google.com => false,
+// with google.com:443 => false,
 func IsValidHostname(hostname string) bool {
-	if strings.EqualFold(hostname, "localhost") {
-		return true
-	}
 	h, err := idna.Lookup.ToASCII(hostname)
 	return err == nil && len(h) > 0
 }
---
+++ validation/socket_test.go
@@ -0,0 +1,88 @@
+package validation
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsValidIP(t *testing.T) {
+	tests := []struct {
+		ip   string
+		want bool
+	}{
+		{
+			ip:   "127.0.0.1",
+			want: true,
+		},
+		{
+			ip:   "::ffff:192.0.2.1",
+			want: true,
+		},
+		{
+			ip:   "127.0.0.1:8080",
+			want: false,
+		},
+		{
+			ip:   "localhost",
+			want: false,
+		},
+		{
+			ip:   "",
+			want: false,
+		},
+		{
+			ip:   "127",
+			want: false,
+		},
+		{
+			ip:   "127.0",
+			want: false,
+		},
+		{
+			ip:   "127.0.0",
+			want: false,
+		},
+		{
+			ip:   "127.0.0.1/32",
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.ip, func(t *testing.T) {
+			if got := IsValidIP(tt.ip); got != tt.want {
+				t.Errorf("IsValidIP() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsValidPort(t *testing.T) {
+	tests := []struct {
+		port int
+		want bool
+	}{
+		{
+			port: 1,
+			want: true,
+		},
+		{
+			port: 65535,
+			want: true,
+		},
+		{
+			port: 0,
+			want: false,
+		},
+		{
+			port: 65536,
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%d", tt.port), func(t *testing.T) {
+			if got := IsValidPort(tt.port); got != tt.want {
+				t.Errorf("IsValidPort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
