c5a27734c7a3e859a1ceaf1da4531173f4020160
VictorTrustyDev <me@victortrusty.dev>
2022-11-23 23:29:54 +0700

    add utils and test

--- utils/number_util.go
+++ utils/number_util.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"fmt"
+	"math/big"
+	"strings"
+)
+
 // ConditionalInt64 returns int64 based on input expression
 func ConditionalInt64(expression bool, whenTrue, whenFalse int64) int64 {
 	if expression {
@@ -16,4 +22,72 @@ func ConditionalInt(expression bool, whenTrue, whenFalse int) int {
 	} else {
 		return whenFalse
 	}
+}
+
+const validHexCharacters = "0123456789abcdefABCDEF"
+
+// IsValidHexNumber returns true if input is a valid hex
+func IsValidHexNumber(hexNumberStr string) bool {
+	startIdx := 0
+	if len(hexNumberStr) < 1 {
+		return false
+	} else if len(hexNumberStr) == 1 {
+		return strings.Index(validHexCharacters, hexNumberStr) >= 0
+	} else if len(hexNumberStr) == 2 {
+		return strings.Index(validHexCharacters, hexNumberStr[0:1]) >= 0 && strings.Index(validHexCharacters, hexNumberStr[1:]) >= 0
+	} else if hexNumberStr[1] == 'x' || hexNumberStr[1] == 'X' {
+		if hexNumberStr[0] != '0' {
+			return false
+		}
+
+		startIdx = 2
+	}
+
+	for i := startIdx; i < len(hexNumberStr); i++ {
+		c := hexNumberStr[i]
+		if c >= 48 && c <= 57 { // 0-9
+			continue
+		}
+		if c >= 65 && c <= 70 { // A-F
+			continue
+		}
+		if c >= 97 && c <= 102 { // a-f
+			continue
+		}
+		return false
+	}
+
+	return true
+}
+
+// ConvertFromHexNumberStringToDecimalString returns decimal representation in string value of input hex value
+func ConvertFromHexNumberStringToDecimalString(hexNumberStr string) (string, error) {
+	return convertFromHexNumberStringToDecimalString(hexNumberStr, false)
+}
+
+// convertFromHexNumberStringToDecimalString returns decimal representation in string value of input hex value
+func convertFromHexNumberStringToDecimalString(hexNumberStr string, bypassValidation bool) (string, error) {
+	if !bypassValidation {
+		if len(hexNumberStr) > 0 && strings.Index(hexNumberStr, "-") == 0 {
+			return "", fmt.Errorf("support positive number only")
+		}
+		if !IsValidHexNumber(hexNumberStr) {
+			return "", fmt.Errorf("not a valid hex")
+		}
+	}
+
+	bi := new(big.Int)
+	var success bool
+
+	if len(hexNumberStr) > 1 && (hexNumberStr[1] == 'x' || hexNumberStr[1] == 'X') {
+		bi, success = bi.SetString(hexNumberStr[2:], 16)
+	} else {
+		bi, success = bi.SetString(hexNumberStr, 16)
+	}
+
+	if !success {
+		return "", fmt.Errorf("unable to set hex number [%s] to *big.Int", hexNumberStr)
+	}
+
+	return bi.String(), nil
 }
--- utils/number_util_test.go
+++ utils/number_util_test.go
@@ -1,6 +1,10 @@
 package utils
 
-import "testing"
+import (
+	"github.com/EscanBE/go-lib/test_utils"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestConditionalInt64(t *testing.T) {
 	tests := []struct {
@@ -62,6 +66,141 @@ func TestConditionalInt(t *testing.T) {
 			if got := ConditionalInt(tt.expression, tt.whenTrue, tt.whenFalse); got != tt.want {
 				t.Errorf("ConditionalInt() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestIsValidHexNumber(t *testing.T) {
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{
+			input: "",
+			want:  false,
+		},
+		{
+			input: "invalid",
+			want:  false,
+		},
+		{
+			input: "F",
+			want:  true,
+		},
+		{
+			input: "G",
+			want:  false,
+		},
+		{
+			input: "f",
+			want:  true,
+		},
+		{
+			input: "g",
+			want:  false,
+		},
+		{
+			input: "0xf",
+			want:  true,
+		},
+		{
+			input: "0xg",
+			want:  false,
+		},
+		{
+			input: "0x01234567890AbCDEF00",
+			want:  true,
+		},
+		{
+			input: "0x01234567890ABCDEFg0",
+			want:  false,
+		},
+		{
+			input: "0Xf",
+			want:  true,
+		},
+		{
+			input: "xf",
+			want:  false,
+		},
+		{
+			input: "AxfF",
+			want:  false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			assert.Equalf(t, tt.want, IsValidHexNumber(tt.input), "IsValidHexNumber(%v)", tt.input)
+		})
+	}
+}
+
+func TestConvertFromHexNumberStringToDecimalString(t *testing.T) {
+	tests := []struct {
+		input            string
+		want             string
+		wantErrMsg       string
+		bypassValidation bool
+	}{
+		{
+			input:      "0xG",
+			want:       "",
+			wantErrMsg: "not a valid hex",
+		},
+		{
+			input: "0XF",
+			want:  "15",
+		},
+		{
+			input: "0XFF",
+			want:  "255",
+		},
+		{
+			input: "FF",
+			want:  "255",
+		},
+		{
+			input: "0x0FFff",
+			want:  "65535",
+		},
+		{
+			input:      "-F",
+			wantErrMsg: "support positive number only",
+		},
+		{
+			input:      "xFF",
+			wantErrMsg: "not a valid hex",
+			// bypassValidation: true, // if bypass, big.Int can still parse this
+		},
+		{
+			input:      "AxfF",
+			wantErrMsg: "not a valid hex",
+			// bypassValidation: true, // if bypass, big.Int can still parse this
+		},
+		{
+			input:            "AxfFAg",
+			wantErrMsg:       "unable to set hex number",
+			bypassValidation: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			var got string
+			var err error
+			if tt.bypassValidation {
+				got, err = convertFromHexNumberStringToDecimalString(tt.input, true)
+			} else {
+				got, err = ConvertFromHexNumberStringToDecimalString(tt.input)
+			}
+			wantErr := len(tt.wantErrMsg) > 0
+			if (err != nil) != wantErr {
+				t.Errorf("got err %v (%s), want %v", err, got, wantErr)
+				return
+			}
+			if err != nil && !test_utils.WantErrorContainsStringIfNonEmptyOtherWiseNoError(t, err, tt.wantErrMsg) {
+				return
+			}
+			assert.Equalf(t, tt.want, got, "got %s, want %s", got, tt.want)
 		})
 	}
 }
