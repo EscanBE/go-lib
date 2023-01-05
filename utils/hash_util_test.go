a2c380640b62bec867f7b3c5590605e6aba6c437
VictorTrustyDev <me@victortrusty.dev>
2023-01-06 03:05:45 +0700

    add test

--- utils/hash_util_test.go
+++ utils/hash_util_test.go
@@ -11,6 +11,10 @@ func TestKeccak256Hash(t *testing.T) {
 			input: "abcd",
 			want:  "48bed44d1bcd124a28c27f343a817e5f5243190d3c52bf347daf876de1dbbf77",
 		},
+		{
+			input: "",
+			want:  "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {
