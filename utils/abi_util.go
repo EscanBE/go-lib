d4729df525659a0757d90750f843a593e3c6d11f
VictorTrustyDev <me@victortrusty.dev>
2023-03-03 14:18:09 +0700

    add some SmartContract/Abi utility methods

---
+++ utils/abi_util.go
@@ -0,0 +1,128 @@
+package utils
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"math"
+	"math/big"
+)
+
+// FromContractResponseStringToAddress convert response from smart contract response into address.
+// CONTRACT: must be valid 32 bytes hex with 0x prefix
+func FromContractResponseStringToAddress(input string) (common.Address, error) {
+	var addr common.Address
+	if len(input) != 66 {
+		return addr, fmt.Errorf("must be 32 bytes data")
+	}
+
+	for i, c := range input[2:27] {
+		if i < 24 {
+			if c != '0' {
+				return addr, fmt.Errorf("first 12 bytes must be zero")
+			}
+		}
+	}
+
+	addr = common.HexToAddress(input)
+
+	return addr, nil
+}
+
+// FromContractResponseBufferToAddress convert response from smart contract response into address.
+func FromContractResponseBufferToAddress(input []byte) (common.Address, error) {
+	var addr common.Address
+	if len(input) != 32 {
+		return addr, fmt.Errorf("must be 32 bytes data")
+	}
+
+	for _, c := range input[:12] {
+		if c != 0 {
+			return addr, fmt.Errorf("first 12 bytes must be zero")
+		}
+	}
+
+	addr = common.BytesToAddress(input)
+
+	return addr, nil
+}
+
+// FromContractResponseStringToHash convert response from smart contract response into hash.
+// CONTRACT: must be valid 32 bytes hex with 0x prefix
+func FromContractResponseStringToHash(input string) (common.Hash, error) {
+	var hash common.Hash
+	if len(input) != 66 {
+		return hash, fmt.Errorf("must be 32 bytes data")
+	}
+
+	hash = common.HexToHash(input)
+
+	return hash, nil
+}
+
+// FromContractResponseBufferToHash convert response from smart contract response into hash.
+func FromContractResponseBufferToHash(input []byte) (common.Hash, error) {
+	var hash common.Hash
+	if len(input) != 32 {
+		return hash, fmt.Errorf("must be 32 bytes data")
+	}
+
+	hash = common.BytesToHash(input)
+
+	return hash, nil
+}
+
+// FromContractResponseStringToString convert response from smart contract response into string.
+// CONTRACT: must be valid hex with 0x prefix
+func FromContractResponseStringToString(input string) (string, error) {
+	if len(input) < 2+32*2*2 {
+		return "", fmt.Errorf("must be at least 64 bytes data")
+	}
+
+	buffer, err := hex.DecodeString(input[2:])
+	if err != nil {
+		return "", fmt.Errorf("bad input")
+	}
+
+	return FromContractResponseBufferToString(buffer)
+}
+
+// FromContractResponseBufferToString convert response from smart contract response into string.
+func FromContractResponseBufferToString(buffer []byte) (string, error) {
+	if len(buffer) < 64 {
+		return "", fmt.Errorf("must be at least 64 bytes data")
+	}
+
+	offset := new(big.Int)
+
+	offset = offset.SetBytes(buffer[:32])
+
+	if offset.Int64() != 32 {
+		return "", fmt.Errorf("value of first 32 bytes must be 32")
+	}
+
+	offset = offset.SetBytes(buffer[32:64])
+
+	strLength := offset.Int64()
+
+	groupsOf32Bytes := int(math.Ceil(float64(strLength) / 32))
+
+	expectedInputBufferLength := 32 + 32 + 32*groupsOf32Bytes
+	if len(buffer) != expectedInputBufferLength {
+		return "", fmt.Errorf("bad input buffer length, got %d, want %d", len(buffer), expectedInputBufferLength)
+	}
+
+	expectedZeroSinceIdx := int(32 + 32 + strLength)
+	for i, b := range buffer {
+		if i < expectedZeroSinceIdx {
+			continue
+		}
+		if b != 0 {
+			return "", fmt.Errorf("un-expected a non-zero bytes at idx %d = %x", i, b)
+		}
+	}
+
+	bufferOfValue := buffer[64 : 64+strLength]
+
+	return string(bufferOfValue), nil
+}
---
+++ utils/abi_util_test.go
@@ -0,0 +1,272 @@
+package utils
+
+import (
+	"encoding/hex"
+	"strings"
+	"testing"
+)
+
+func TestFromContractResponseStringToAddress(t *testing.T) {
+	tests := []struct {
+		name    string
+		address string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "normal",
+			address: "0x000000000000000000000000ae38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			want:    "0xae38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			wantErr: false,
+		},
+		{
+			name:    "normal with capital 0x",
+			address: "0X000000000000000000000000ae38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			want:    "0xae38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			wantErr: false,
+		},
+		{
+			name:    "normal with capital",
+			address: "0x000000000000000000000000AE38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			want:    "0xae38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			wantErr: false,
+		},
+		{
+			name:    "not enough 32 bytes",
+			address: "0x000000000000000000000000ae38558e1cccf2d930e7be45de62bcc95c32b0",
+			wantErr: true,
+		},
+		{
+			name:    "first 12 bytes must be zero",
+			address: "0x000000000000000000000001ae38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			wantErr: true,
+		},
+		{
+			name:    "first 12 bytes must be zero",
+			address: "0x100000000000000000000000ae38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			wantErr: true,
+		},
+		{
+			name:    "all zero returns empty addr without err",
+			address: "0x0000000000000000000000000000000000000000000000000000000000000000",
+			want:    "0x0000000000000000000000000000000000000000",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FromContractResponseStringToAddress(tt.address)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FromContractResponseStringToAddress() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err == nil {
+				if !strings.EqualFold(got.String(), tt.want) {
+					t.Errorf("FromContractResponseStringToAddress() got = %v, want %v", got, tt.want)
+				}
+			} else {
+				if got.String() != "0x0000000000000000000000000000000000000000" {
+					t.Errorf("FromContractResponseStringToAddress() must returns empty addr on error, but got %s", got)
+				}
+			}
+
+			// test buffer
+			buffer, err2 := hex.DecodeString(tt.address[2:])
+			if err2 != nil {
+				t.Errorf("failed to decode input addr %s into buffer for testing", tt.address)
+				t.FailNow()
+			}
+			addr2, err2 := FromContractResponseBufferToAddress(buffer)
+			if err == nil {
+				if err2 != nil {
+					t.Errorf("FromContractResponseBufferToAddress() expects no error but got %v", err2)
+					t.FailNow()
+				}
+
+				if addr2 != got {
+					t.Errorf("result from buffer method is different with result from string method: got = %v, want = %v", addr2, got)
+				}
+			} else {
+				if err2 == nil {
+					t.Errorf("FromContractResponseBufferToAddress() expects error but got no error")
+					t.FailNow()
+				}
+
+				if addr2.String() != "0x0000000000000000000000000000000000000000" {
+					t.Errorf("FromContractResponseBufferToAddress() must returns empty addr on error, but got %s", addr2)
+				}
+			}
+		})
+	}
+}
+
+func TestFromContractResponseStringToHash(t *testing.T) {
+	tests := []struct {
+		name    string
+		hash    string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "normal",
+			hash:    "0x000000000000000000000000ae38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			want:    "0x000000000000000000000000ae38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			wantErr: false,
+		},
+		{
+			name:    "normal with capital 0x",
+			hash:    "0X000000000000000000000000ae38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			want:    "0x000000000000000000000000ae38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			wantErr: false,
+		},
+		{
+			name:    "normal with capital",
+			hash:    "0x000000000000000000000000AE38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			want:    "0x000000000000000000000000ae38558e1cccf2d930e7be45de62bcc95c32b0ef",
+			wantErr: false,
+		},
+		{
+			name:    "not enough 32 bytes",
+			hash:    "0x000000000000000000000000ae38558e1cccf2d930e7be45de62bcc95c32b0",
+			wantErr: true,
+		},
+		{
+			name: "all zero returns empty addr without err",
+			hash: "0x0000000000000000000000000000000000000000000000000000000000000000",
+			want: "0x0000000000000000000000000000000000000000000000000000000000000000",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FromContractResponseStringToHash(tt.hash)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FromContractResponseStringToHash() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err == nil {
+				if !strings.EqualFold(got.String(), tt.want) {
+					t.Errorf("FromContractResponseStringToHash() got = %v, want %v", got, tt.want)
+				}
+			} else {
+				if got.String() != "0x0000000000000000000000000000000000000000000000000000000000000000" {
+					t.Errorf("FromContractResponseStringToHash() must returns empty hash on error, but got %s", got)
+				}
+			}
+
+			// test buffer
+			buffer, err2 := hex.DecodeString(tt.hash[2:])
+			if err2 != nil {
+				t.Errorf("failed to decode input addr %s into buffer for testing", tt.hash)
+				t.FailNow()
+			}
+			hash2, err2 := FromContractResponseBufferToHash(buffer)
+			if err == nil {
+				if err2 != nil {
+					t.Errorf("FromContractResponseBufferToHash() expects no error but got %v", err2)
+					t.FailNow()
+				}
+
+				if hash2 != got {
+					t.Errorf("result from buffer method is different with result from string method: got = %v, want = %v", hash2, got)
+				}
+			} else {
+				if err2 == nil {
+					t.Errorf("FromContractResponseBufferToHash() expects error but got no error")
+					t.FailNow()
+				}
+
+				if hash2.String() != "0x0000000000000000000000000000000000000000000000000000000000000000" {
+					t.Errorf("FromContractResponseBufferToHash() must returns empty hash on error, but got %s", hash2)
+				}
+			}
+		})
+	}
+}
+
+func TestFromContractResponseStringToString(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "normal",
+			input:   "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000d68d0b56c6cd0be2e65766d6f7300000000000000000000000000000000000000",
+			want:    "hеllо.evmos",
+			wantErr: false,
+		},
+		{
+			name:    "empty",
+			input:   "0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000000",
+			want:    "",
+			wantErr: false,
+		},
+		{
+			name:    "non-zero after str data",
+			input:   "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000d68d0b56c6cd0be2e65766d6f7311000000000000000000000000000000000000",
+			wantErr: true,
+		},
+		{
+			name:    "buffer size not match",
+			input:   "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000d68d0b56c6cd0be2e65766d6f73000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+			wantErr: true,
+		},
+		{
+			name:    "buffer size not match",
+			input:   "0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000001",
+			wantErr: true,
+		},
+		{
+			name:    "first 32 bytes value must be 32",
+			input:   "0x0000000000000000000000000000000000000000000000000000000000000021000000000000000000000000000000000000000000000000000000000000000d68d0b56c6cd0be2e65766d6f7300000000000000000000000000000000000000",
+			wantErr: true,
+		},
+		{
+			name:    "shorter than 64 bytes",
+			input:   "0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FromContractResponseStringToString(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FromContractResponseStringToString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err == nil && got != tt.want {
+				t.Errorf("FromContractResponseStringToString() got = %v, want %v", got, tt.want)
+			}
+			if err != nil && got != "" {
+				t.Errorf("FromContractResponseStringToString() expected empty result when null, but got %s", got)
+			}
+
+			// test buffer
+			buffer, err2 := hex.DecodeString(tt.input[2:])
+			if err2 != nil {
+				t.Errorf("failed to decode input addr %s into buffer for testing", tt.input)
+				t.FailNow()
+			}
+			value2, err2 := FromContractResponseBufferToString(buffer)
+			if err == nil {
+				if err2 != nil {
+					t.Errorf("FromContractResponseBufferToString() expects no error but got %v", err2)
+					t.FailNow()
+				}
+
+				if value2 != got {
+					t.Errorf("result from buffer method is different with result from string method: got = %v, want = %v", value2, got)
+				}
+			} else {
+				if err2 == nil {
+					t.Errorf("FromContractResponseBufferToString() expects error but got no error")
+					t.FailNow()
+				}
+
+				if value2 != "" {
+					t.Errorf("FromContractResponseBufferToString() must returns empty string on error, but got %s", value2)
+				}
+			}
+		})
+	}
+}
