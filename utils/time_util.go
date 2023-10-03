972a08a2d49012c5c91744f27cff419dc81b9736
VictorTrustyDev <me@victortrusty.dev>
2023-10-03 09:46:43 +0700

    add some time util

--- utils/time_util.go
+++ utils/time_util.go
@@ -1,6 +1,10 @@
 package utils
 
-import "time"
+import (
+	"fmt"
+	"math"
+	"time"
+)
 
 // NowS returns the current epoch seconds
 func NowS() int64 {
@@ -20,4 +24,35 @@ func DiffS(previous int64) int64 {
 // DiffMs returns offset amount between the current epoch milliseconds and supplied epoch from the `previous` arg
 func DiffMs(previous int64) int64 {
 	return NowMs() - previous
+}
+
+// GetLocationFromUtcTimezone returns location corresponding to specified UTC-based timezone
+func GetLocationFromUtcTimezone(utcTimezone int) *time.Location {
+	ensureUtcTimezone(utcTimezone)
+	return time.FixedZone(GetUtcName(utcTimezone), utcTimezone*60*60)
+}
+
+// GetUtcName returns naming convention of UTC timezone. Eg: 7 => UTC+0700
+func GetUtcName(utcTimezone int) string {
+	ensureUtcTimezone(utcTimezone)
+	return fmt.Sprintf("UTC%s", getTimezoneSuffix(utcTimezone))
+}
+
+// ensureUtcTimezone will panic if timezone is out of range from -12 to 14
+func ensureUtcTimezone(utcTimezone int) {
+	if utcTimezone < -12 || utcTimezone > 14 {
+		panic(fmt.Errorf("UTC timezone must be in range -12 to 14"))
+	}
+}
+
+func getTimezoneSuffix(timezone int) string {
+	if timezone > 9 {
+		return fmt.Sprintf("+%d00", timezone)
+	} else if timezone >= 0 {
+		return fmt.Sprintf("+0%d00", timezone)
+	} else if timezone >= -9 {
+		return fmt.Sprintf("-0%d00", int(math.Abs(float64(timezone))))
+	} else {
+		return fmt.Sprintf("%d00", timezone)
+	}
 }
--- utils/time_util_test.go
+++ utils/time_util_test.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"github.com/EscanBE/go-lib/test_utils"
 	"testing"
 	"time"
 )
@@ -43,5 +44,82 @@ func TestNowS(t *testing.T) {
 	diff := AbsInt64(nowS1 - nowS2)
 	if diff > 1 {
 		t.Errorf("NowS() = %v, expected %v +-1!", nowS1, nowS2)
+	}
+}
+
+func TestGetLocationFromUtcTimezone(t *testing.T) {
+	t.Run("get location for UTC from -12 to 14", func(_ *testing.T) {
+		for timezone := -12; timezone <= 14; timezone++ {
+			loc := GetLocationFromUtcTimezone(timezone)
+			nowUTC := time.Date(2023, 9, 16, 0, 0, 0, 0, time.UTC)
+			nowWithCustomLoc := nowUTC.In(loc)
+			diffHours := (nowWithCustomLoc.Hour() + 24*nowWithCustomLoc.Day()) - (nowUTC.Hour() + 24*nowUTC.Day())
+			if diffHours != timezone {
+				t.Errorf("Expected diff timezone %d but got %d", timezone, diffHours)
+			}
+		}
+	})
+
+	for timezone := -100; timezone <= 100; timezone++ {
+		wantPanic := timezone < -12 || timezone > 14
+		t.Run(fmt.Sprintf("timezone %d %s", timezone, func() string {
+			if wantPanic {
+				return "should panic"
+			} else {
+				return "should not panic"
+			}
+		}()), func(_ *testing.T) {
+			defer test_utils.DeferWantPanicDepends(t, wantPanic)
+			_ = GetLocationFromUtcTimezone(timezone)
+		})
+	}
+}
+
+func TestGetUtcName(t *testing.T) {
+	tests := []struct {
+		utcTimezone int
+		want        string
+	}{
+		{
+			utcTimezone: -12,
+			want:        "UTC-1200",
+		},
+		{
+			utcTimezone: -1,
+			want:        "UTC-0100",
+		},
+		{
+			utcTimezone: 0,
+			want:        "UTC+0000",
+		},
+		{
+			utcTimezone: 1,
+			want:        "UTC+0100",
+		},
+		{
+			utcTimezone: 14,
+			want:        "UTC+1400",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.want, func(t *testing.T) {
+			if got := GetUtcName(tt.utcTimezone); got != tt.want {
+				t.Errorf("GetUtcName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+	for timezone := -100; timezone <= 100; timezone++ {
+		wantPanic := timezone < -12 || timezone > 14
+		t.Run(fmt.Sprintf("timezone %d %s", timezone, func() string {
+			if wantPanic {
+				return "should panic"
+			} else {
+				return "should not panic"
+			}
+		}()), func(_ *testing.T) {
+			defer test_utils.DeferWantPanicDepends(t, wantPanic)
+			_ = GetUtcName(timezone)
+		})
 	}
 }
